49973336e2d44060a88f6a53ccf17c5e6b7ccdc2
rusweek <rusweek@gmail.com>
2019-12-07 17:10:35 +0700

    подключена страница homePage к redux store

--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import BookList from "../book-list";
 
 const HomePage = () =>{
-    return <div>Home page</div>
+
+    return (
+        <BookList />
+    )
 };
 
 export default HomePage;
